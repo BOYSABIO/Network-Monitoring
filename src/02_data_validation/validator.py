bec2594af737de48c42fd0e504bb787a867db6bb
BOYSABIO <129614198+BOYSABIO@users.noreply.github.com>
2025-12-04 22:42:59 +0100

    Fixing libraries & structure

--- src/02_data_validation/validator.py
+++ src/02_data_validation/validator.py
@@ -8,20 +8,21 @@ import pandas as pd
 import numpy as np
 from src.data_load.data_loader import load_data
 
-def data_validator():
+def data_validator(df):
     '''
     Valdate data after loading
     '''
     try:
         logging.info("Validating data after loading")
-        df = load_data()
+        
         logging.info("Data validated successfully")
     except Exception as e:
         logging.error(f"Error validating data: {e}")
         raise e
 
 def main():
-    data_validator()
+    df = load_data()
+    data_validator(df)
 
 if __name__ == "__main__":
     main()
